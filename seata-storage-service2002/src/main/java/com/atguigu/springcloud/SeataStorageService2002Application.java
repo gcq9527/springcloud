f0788902d9aa20b5fb48ff365543423076157753
gcq9527 <1950105527@qq.com>
2020-08-19 15:06:28 +0800

    初次提交

--- seata-storage-service2002/src/main/java/com/atguigu/springcloud/SeataStorageService2002Application.java
+++ seata-storage-service2002/src/main/java/com/atguigu/springcloud/SeataStorageService2002Application.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //取消数据源自动创建
 public class SeataStorageService2002Application {
-
     public static void main(String[] args) {
         SpringApplication.run(SeataStorageService2002Application.class, args);
     }
